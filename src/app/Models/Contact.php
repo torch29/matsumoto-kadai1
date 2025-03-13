bd9066f9a357de4ad3ed120d29c56d2779eeb111
Torch29 <ashtonerico@yahoo.co.jp>
2025-03-13 23:28:41 +0900

    管理画面の検索入力フォームに完全一致による検索機能を追加

--- src/app/Models/Contact.php
+++ src/app/Models/Contact.php
@@ -28,10 +28,19 @@ class Contact extends Model
 
     public function scopeKeywordSearch($query, $keyword) {
         if (!empty($keyword)) {
-            $query->where('first_name', 'like', '%' . $keyword . '%')
-            ->orWhere('last_name', 'like', '%' . $keyword . '%')
-            ->orWhere('email', 'like', '%' . $keyword . '%');
-            //->orWhere('last_name' . 'first_name' , 'like' , $keyword);
+            $query->where(function ($q) use ($keyword) {
+                $q->where('last_name', 'like', '%' . $keyword . '%')
+                ->orWhere('first_name', 'like', '%' . $keyword . '%')
+                ->orWhere('email', 'like', '%' . $keyword . '%');
+
+                $keywords = preg_split('/\s+/u', $keyword, -1, PREG_SPLIT_NO_EMPTY); //空白文字があるたびに区切る
+                if (count($keywords) === 2) {
+                $q->orWhere(function ($q2) use ($keywords) {
+                    $q2->where('last_name', $keywords[0])
+                    ->where('first_name', $keywords[1]);
+                    });
+                }
+            });
         }
     }
     public function scopeDateSearch($query, $date) {
@@ -45,8 +54,9 @@ class Contact extends Model
         }
     }
     public function scopeGenderSearch($query, $gender_select) {
+        //空でない＆9（全て）が選択されていないとき
         if (!empty($gender_select) && $gender_select != 9) {
-            $query->where('gender', $gender_select)->get();
+            $query->where('gender', $gender_select);
         }
     }
 }
