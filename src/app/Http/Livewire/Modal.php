0b6502136d0f93cb8e09b745a52e4252d32e8536
Torch29 <ashtonerico@yahoo.co.jp>
2025-03-16 23:54:50 +0900

    コードの整理

--- src/app/Http/Livewire/Modal.php
+++ src/app/Http/Livewire/Modal.php
@@ -20,22 +20,9 @@ class Modal extends Component
     use WithPagination;
         protected $paginationTheme = 'bootstrap';
 
-    //public $contacts;
 
     public function wireSearch()
     {
-        //dd($this->keyword, $this->date, $this->category_select, $this->gender_select);
-        
-        /*
-        $contacts = Contact::with('category')
-        ->KeywordSearch('keyword')
-        ->DateSearch('date')
-        ->CategorySearch('category_select')
-        ->GenderSearch('gender_select')
-        ->Paginate(7);
-        */
-
-        /*$this->contacts = Contact::with('category')*/
 
         $contacts = Contact::with('category')->KeywordSearch($this->keyword)
         ->DateSearch($this->date)
@@ -43,16 +30,10 @@ class Modal extends Component
         ->GenderSearch($this->gender_select)
         ->paginate(7);
 
-        //dd($this->keyword, $this->date, $this->category_select, $this->gender_select);  // デバッグ
-
     }
 
         public function render()
     {
-
-        //return view('livewire.modal');
-        //初期データがない場合に表示
-        /*$contacts = $this->contacts ?? Contact::with('category')->Paginate(7);*/
 
         $contacts = Contact::with('category')->Paginate(7);
         $categories = Category::all();
@@ -61,26 +42,6 @@ class Modal extends Component
             2 => '女性',
             3 => 'その他'
         ];
-
-            // searchメソッドが呼び出されていない場合は全てのデータを表示
-        /*
-        if (!isset($this->contacts)) {
-            $contacts = Contact::with('category')->paginate(7);
-        } else {
-            $contacts = $this->contacts;
-        }
-        */
-
-        /*
-        if ($this->keyword) {
-        $contacts = Contact::with('category')
-            ->KeywordSearch($this->keyword)
-            ->DateSearch($this->date)
-            ->CategorySearch($this->category_select)
-            ->GenderSearch($this->gender_select)
-            ->Paginate(7);
-        }
-            */
 
         return view('livewire.modal', compact('contacts', 'genders', 'categories'));
     }
